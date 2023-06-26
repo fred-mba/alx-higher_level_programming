a50acdfb78f3d06c364fb7eef99e57658d6f7451
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-26 12:52:59 +0000

    Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-words = str.split()  # Split the string into a list of words
-str = " ".join([words[5], words[6], words[7], words[0]])  # Concatenate the desired words
+str = str[39:67] + str[107:112] + str[:6]
 print(str)
