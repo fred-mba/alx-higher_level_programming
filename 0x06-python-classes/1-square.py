c8d50dfe7aebfb1307831025d7e5783f59f23d81
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-27 20:50:35 +0000

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""
-Provides a simple Square class with initialize size
-"""
 
 
-class Square:
-     """class Square that defines a square by size"""
-    def __init__(self, size):
+class Square():
+     """Class Square, manage validation"""
+        def __init__(self, size=0):
+        if size != int(size):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
