d49e3e376c4db55215ca476238cd882017721e09
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-27 12:12:44 +0000

    To uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for char in str:
+        uppercase_char = chr(ord(char) - 32) if 97 <= ord(char) <= 122 else char
+        print(uppercase_char, end='')
+    print()
