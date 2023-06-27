51a6c82b3762d34a42c47df30d140c38237fc678
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-27 21:05:03 +0000

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-"""This is the "Square"  module"""
+"""
+This is the "Square"  module.
 
+The module provides a simple Square class with initialize size.
+"""
 
 class Square():
 """Class Square, manage validation"""
