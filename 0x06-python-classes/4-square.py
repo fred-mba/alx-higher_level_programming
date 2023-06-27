d314b5d97d1ff60b43eaa4b65eafe599a5cd3ac5
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-27 21:30:20 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+"Square"  module.
+
+Provides a simple Square class with initialize size.
+if size is less than 0, raise aValueError exception with
+the message size must be >= 0
+"""
+
+
+class Square:
+    """This class defines a square by size and can compute area"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
