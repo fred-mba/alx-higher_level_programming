afec35c825f20a94dce1776a3b192d81bb32790c
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-27 16:47:40 +0000

    Smile in the mirror

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(122, 96, -1):
-    char = chr(i)
+    char = i
     if i % 2 != 0:
-        char = char.upper()
-    print(char, end='')
+        char = char - 32
+    print("{:c}".format(char), end='')
