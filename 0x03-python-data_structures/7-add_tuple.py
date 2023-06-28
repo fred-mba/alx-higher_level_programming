7ab84073e4ea3e69379e49f4f0d0c5df71e9141b
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-28 11:58:01 +0000

    Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """
+    Adds two tuples and returns a new tuple with the sums of corresponding elements.
+    """
+    tuple_a = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    tuple_b = tuple_b + (0, 0)[:2 - len(tuple_b)]
+
+    sum_a = tuple_a[0] + tuple_b[0]
+    sum_b = tuple_a[1] + tuple_b[1]
+
+    return sum_a, sum_b
