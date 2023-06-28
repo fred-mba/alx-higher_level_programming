33f60036b11a6c1f540e207c4386b59168d83845
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-06-28 07:20:43 +0000

    Who are you?

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    
+    all_dir = [name for name in dir(hidden_4) if not name.startswith("__")]
+    
+    for name in all_dir:
+        print(name)
