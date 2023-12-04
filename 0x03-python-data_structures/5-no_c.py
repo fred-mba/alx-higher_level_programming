6909f4adb62b942e3be18c4a495f0246a767eabb
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-12-04 17:20:07 +0000

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/bin/bash/python3
+
+
+def no_c(my_string):
+    modified_string = ''
+
+    for letter in my_string:
+        if letter.lower() != 'c':
+            modified_string += letter
+
+    return modified_string
