d6df95065c463c3ed6580b89e7735681dc236872
fredrick mbanga <fredrickmbangah68@gmail.com>
2023-12-05 02:17:26 +0000

    Only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    """
+    Finds all multiples of 2 in a list
+    Returns True if an element is a multiple of 2, else false
+    """
+    new_list = []
+
+    for num in my_list:
+        if num % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
