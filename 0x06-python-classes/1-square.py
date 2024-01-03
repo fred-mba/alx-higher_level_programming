a80dc3fd5c4a8ee059e7dddf614586e2f560c15b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-01-03 13:27:51 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square with a private size attribute
+
+   The Square class is instantiated with size parameter"""
+
+
+class Square:
+    """A class to represent a square"""
+    def __init__(self, size):
+        self.__size = size
