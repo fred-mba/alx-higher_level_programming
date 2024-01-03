60618b46404b782904d195fb5431548237e64314
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-01-03 17:51:58 +0300

    Printing a square

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a square with private attribute size initialized to 0 by default
+
+   Prints in stdout the square with the character `#`
+"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        for i in range(self.__size):
+            print("#" * self.__size)
