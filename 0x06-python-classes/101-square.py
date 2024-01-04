1a791620f979cd2a48e67a44835b5773d0041008
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-01-04 07:23:03 +0300

    Print Square instance

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -50,18 +50,24 @@ class Square:
         if self.__size == 0:
             print("")
 
-        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__position[1]):
+            print("")
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
+            for j in range(0, self.__position[0]):
+                print(" ", end="")
+            for k in range(0, self.__size):
+                print("#", end="")
             print("")
 
     def __str__(self):
         if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
+            for i in range(0, self.__position[1]):
+                 print("")
         for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
+            for j in range(0, self.__position[0]):
+                 print(" ", end="")
+            for k in range(0, self.__size):
+                 print("#", end="")
             if i != self.__size - 1:
                 print("")
         return ("")
