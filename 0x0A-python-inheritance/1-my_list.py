53358442f32fb2d8ec672ea453430d99bd217b99
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-01-09 05:38:42 +0300

    My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class MyList(list):
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        sorted_list = sorted(self)
+        print(sorted_list)
