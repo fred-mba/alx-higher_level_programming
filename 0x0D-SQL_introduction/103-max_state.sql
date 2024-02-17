f09f58b0b91440f6d961e28a0dc9ac06f8dfaa9c
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-02-17 23:39:10 +0300

    Temperatures #2

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,7 @@
+-- From imported temperatures db, this script that displays the max 
+  -- temperature of each state (ordered by State name)
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+LIMIT 3;
+
