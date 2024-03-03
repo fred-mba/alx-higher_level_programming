e789a6e53178c8fab292374ac5d7afa809395986
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-03 22:53:40 +0300

    Genre ID by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,6 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
     -- Each record should display: tv_shows.title - tv_show_genres.genre_id
     -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-USE hbtn_0d_tvshows;
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
