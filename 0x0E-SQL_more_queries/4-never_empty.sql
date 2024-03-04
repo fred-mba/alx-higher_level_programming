2167aed030d077d07142fc01d72db7776c001750
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-04 07:08:23 +0300

    ID can't be null

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -4,4 +4,6 @@
 	-- id INT with the default value 1
 	-- name VARCHAR(256)
 -- If the table id_not_null already exists, script should not fail
-CREATE TABLE IF NOT EXISTS `id_not_null` (`id` INT DEFAULT 1, `name` VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS `id_not_null` (
+	`id` INT DEFAULT 1, `name` VARCHAR(256) NOT NULL
+);
