92f60da6695e8d3448ae31ffa250e295aa58fa80
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-04 07:05:14 +0300

    Cities by States

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,6 +3,7 @@
 -- Results must be sorted in ascending order by cities.id
 
 SELECT cities.id, cities.name, states.name
-	FROM cities JOIN states
-	ON cities.state_id = states.id
+	FROM cities
+		JOIN states
+		ON cities.state_id = states.id
 ORDER BY cities.id ASC;
