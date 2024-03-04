4e8d8fd7b22d6075738a326e45dc66976fe58d6d
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-04 19:55:59 +0300

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,8 +3,6 @@
 -- Each record should display: tv_genres.name
 -- Results must be sorted in ascending order by the genre name
 
-USE hbtn_0d_tvshows;
-
 SELECT tv_genres.name FROM tv_show_genres
     JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
