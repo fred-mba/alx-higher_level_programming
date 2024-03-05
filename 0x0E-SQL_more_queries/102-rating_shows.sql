e9ee8b00703548813b3aacd8822bf2aa5daedd59
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-05 23:31:50 +0300

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -2,8 +2,8 @@
 -- Each record should display: tv_shows.title - rating sum
 -- Results must be sorted in descending order by the rating
 
-SELECT tv_shows.title, SUM(ratings.rating) AS rating_sum
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
 FROM tv_shows
-JOIN ratings ON tv_shows.id = ratings.show_id
-GROUP BY tv_shows.id, tv_shows.title
-ORDER BY rating_sum DESC;
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_shows.title
+ORDER BY rating DESC;
