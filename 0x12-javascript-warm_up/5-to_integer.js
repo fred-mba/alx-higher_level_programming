b5a85c015f6bf6db69d5649be0ae4e9f780c0c3c
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-12 05:57:44 +0300

    An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const input = process.argv;
+const number = parseInt(input[2]);
+
+if (isNaN(number)) {
+    console.log('Not a number');
+  } else {
+    console.log(`My number: ${number}`);
+  }
