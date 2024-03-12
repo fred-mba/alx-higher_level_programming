029f4bac6f34ffdbc96dee8c8ab35a9b0679542e
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-12 05:33:34 +0300

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const args = process.argv;
+const arg = process.argv;
 
-if (args.length === 2) {
+if (arg.length === 2) {
   console.log('No argument');
-} else if (args.length === 3) {
+} else if (arg.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
