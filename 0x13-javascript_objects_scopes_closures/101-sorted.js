1bd36bc6cd17ff3ddf019702389375c82d52bd5b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-13 05:25:10 +0300

    Sorted occurences

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const dict = require('./101-data').dict;
+const userOccurrences = {};
+
+for (const userId in dict) {
+  const occurrences = dict[userId];
+
+  if (!userOccurrences[occurrences]) {
+    userOccurrences[occurrences] = [];
+  }
+
+  userOccurrences[occurrences].push(userId);
+}
+
+console.log(userOccurrences);
