d74c5f9762088903a68e75d240448196a4440c91
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-29 05:56:22 +0300

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 """This script fetches https://alx-intranet.hbtn.io/status"""
-from os import error
-from urllib.error import URLError
 import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
-
 if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
     with urllib.request.urlopen(url) as response:
-        html = response.read()
+        content = response.read()
     print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html)
-    print("\t- utf8 content:", html.decode('utf-8'))
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", content.decode('utf-8'))
