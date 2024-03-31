dd044c551dd4e8497aafeb5ee7ac2acd05dc0c35
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-03-31 22:31:26 +0300

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Makes a request on the server to respond with a message containing You got me!
-curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
 
