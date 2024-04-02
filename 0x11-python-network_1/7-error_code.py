2b86febbd1edba2358079e7811f92bc490560da8
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-02 05:57:59 +0300

    Error code #1

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Takes in a URL, sends a request to the URL and displays the body of the
+response.
+If the HTTP status code is greater than or equal to 400, print:
+Error code: followed by the value of the HTTP status code
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+
+    resp = requests.get(url)
+
+    if resp.status_code > 400:
+        print("Errror code:", resp.status_code)
+    else:
+        print(resp.text)
