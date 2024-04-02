0b8ede49b6f1bb0c9375e80e647155064dd512ec
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-02 05:41:38 +0300

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+ Takes in a URL and an email address, sends a POST request to the passed URL
+ with the email as a parameter, and displays the body of the response.
+"""
+from urllib import response
+from xml.sax.handler import DTDHandler
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    theEmail = argv[2]
+
+    respo = requests.post(argv[1], data={'email': argv[2]})
+    print(respo.text)
