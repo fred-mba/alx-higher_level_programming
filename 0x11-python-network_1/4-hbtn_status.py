e55a26b9d69e08433a8bfb52f01e508066242c89
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-02 05:16:06 +0300

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,8 +3,6 @@
 This module fetches https://alx-intranet.hbtn.io/status url
 """
 import requests
-from sys import argv
-
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
