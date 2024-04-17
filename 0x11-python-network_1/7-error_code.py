dbad33593fa568dac2ac2c445549261ad72904a4
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-04-17 15:58:18 +0300

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,11 +9,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
-
-    resp = requests.get(url)
-
-    if resp.status_code > 400:
-        print("Error code:", resp.status_code)
+    url = requests.get(argv[1])
+    if url.status_code >= 400:
+        print("Error code: {}".format(url.status_code))
     else:
-        print(resp.text)
+        print(url.text)
