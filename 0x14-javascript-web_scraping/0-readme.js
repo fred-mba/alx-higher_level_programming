75794e8d5edc401f67167a49e8a530e886e12394
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-02 15:01:34 +0300

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-const filePath = process.argv[2]
+const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
@@ -10,4 +10,4 @@ fs.readFile(filePath, 'utf-8', (err, data) => {
   } else {
     console.log(data);
   }
-}); 
+});
