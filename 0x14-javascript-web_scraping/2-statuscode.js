685fb170cd849c41ec47e1989ffaebab92dbdf8e
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-02 16:15:54 +0300

    Status code

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const req = require('request');
+
+const url = process.argv[2];
+
+req(url, (error, response) => {
+  if (error) console.log(error)
+
+  console.log("code:", response.statusCode);
+});
