da768c0af64f4baa476dfdb0c6d09ce0db3acb60
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-03 22:26:52 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,7 +3,7 @@
 const req = require('request');
 const apiUrl = process.argv[2];
 
-const charUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
+const charId = '18';
 
 req(apiUrl, (error, response, body) => {
   if (error) throw error;
@@ -13,8 +13,10 @@ req(apiUrl, (error, response, body) => {
   let filmCount = 0;
 
   for (const film of filmData.results) {
-    if (film.characters.includes(charUrl)) {
-      filmCount++;
+    for (const characterUrl of film.characters) {
+      if (characterUrl.includes(charId)) {
+        filmCount++;
+      }
     }
   }
 
