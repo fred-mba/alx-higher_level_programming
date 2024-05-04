6c03c0150fb1b743b4e4143b470bd85d19fa5097
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-04 12:16:49 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/**
+* A script that prints the number of movies where the character “Wedge Antilles” is present
+* Wedge Antilles is character ID 18 - the script must use this ID for filtering the result of the API
+* Star wars API: https://swapi-api.alx-tools.com/api/films/
+*/
 
 const req = require('request');
 const apiUrl = process.argv[2];
