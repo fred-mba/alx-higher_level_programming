492320632905e1740f3d116da3ee2e15d3e6a02f
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-04 12:26:10 +0300

    Who was playing in this movie?

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
+/**
+ * A script that prints all characters of a Star Wars movie
+ * The first argument is the Movie ID - example: 3 = “Return of the Jedi”
+ * Display one character name by line
+ * Must use Star ways api: https://swapi-api.alx-tools.com/
+*/
 
 const req = require('request');
 const movieId = process.argv[2];
