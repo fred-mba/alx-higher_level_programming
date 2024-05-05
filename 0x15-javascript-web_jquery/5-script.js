da269b9dea94c695b47ac151e561e1cd1a055e84
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-05-05 15:44:02 +0300

    List of elements

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,3 +1,8 @@
+/**
+ * Adds a <li> element to a list when the user clicks on the tag DIV#add_item
+ * The new element must be: <li>Item</li>
+ * The new element must be added to UL.my_list
+ */
 $('DIV#add_item').on('click', function () {
   $('UL.my_list').append('<li>Item</li>');
 });
