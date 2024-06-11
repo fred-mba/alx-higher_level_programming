8809bc7d85c96cfe6b0db6ee75c6badb9cfe2a60
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-06-11 18:24:58 +0300

    Improved documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """
-This is the add integer module
-
-Arg:
-    a and b must be first casted to integers if they are float
-
+Add integer numbers.
 """
 
 
 def add_integer(a, b=98):
-    """Returns an addition of two integers
+    """
+    Add up two integer values.
 
-       Othewise raise a TypeError"""
+    This function adds one value integer to a default value 98.
+
+    Parameters
+    ----------
+    a : int
+    b : int, default 98
+
+    Returns
+    -------
+    int
+       Summation of two numbers
+    """
 
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
@@ -19,9 +27,9 @@ def add_integer(a, b=98):
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
 
-    for num, name in zip([a, b], ['a', 'b']):
+    for key, value in zip([a, b], ['a', 'b']):
         if isinstance(a, (list, tuple, str)) or \
                 isinstance(b, (list, tuple, str)):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("{} must be an integer".format(value))
 
     return int(a) + int(b)
