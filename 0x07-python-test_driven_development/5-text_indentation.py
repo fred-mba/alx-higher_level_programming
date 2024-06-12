72261e4eac37807cba21dc27c2b1ba76751a2644
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-06-12 16:21:36 +0300

    Text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This implementation prints each text after `.`, `:`, and `?` on a new lines
+"""
+
+
+def text_indentation(text):
+    """
+    Print a text with 2 lines after ., ?, and :.
+
+    The function transverses through each character to find the stated 3
+    characters. If found, the sentence is printed out one one line followed by
+    a new line.
+
+    Parameter
+    ---------
+    text : str
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    special_char = ['.', '?', ':']
+    sentence_line = ""
+
+    for character in text:
+        sentence_line += character
+        if character in special_char:
+            print(sentence_line.strip())
+            print()
+            sentence_line = ""
+
+    if sentence_line:
+        print(sentence_line.strip())
