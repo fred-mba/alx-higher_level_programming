f45b72926df783d9e2a8a35e9826f3bfb719a57b
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2024-06-12 13:01:37 +0300

    Improved docstrings

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """
-This module prints first and last name
-
-Arg:
-    first_name(str), last_name(str)
+This module prints out 1st and last name using say_my_name function
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """Returns (str) of name(s)"""
+    """
+    Print 1st and last name.
+
+    This function takes the two parameters which should print out concantenated
+    string name.
+
+    Parameters
+    ----------
+    first_name : str
+    last_name : str, default whitespace ""
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
