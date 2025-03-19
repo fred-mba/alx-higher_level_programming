13ecb8da7957db2ff5d93bf93ce5bebd4f5bd8f9
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-03-19 15:04:24 +0300

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,20 +1,52 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
+
+
+def print_usage_and_exit():
+    print("Usage: nqueens N")
+    sys.exit(1)
+
+
+def print_error_and_exit(message):
+    print(message)
+    sys.exit(1)
+
+
+def is_safe(board, row, col):
+    """Check if placing a queen at (row, col) is safe"""
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+            return False
+    return True
+
+
+def solve_nqueens(n, row=0, board=[]):
+    """Backtracking function to place queens"""
+    if row == n:
+        print([[i, board[i]] for i in range(n)])
+        return
+    for col in range(n):
+        if is_safe(board, row, col):
+            solve_nqueens(n, row + 1, board + [col])
 
 
 def main():
-    ac = len(argv)
-    if ac != 2:
-        print("Usage: nqueens N")
-        exit(1)
-    elif argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
-    elif int(argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
+    """Main function to handle input validation and call the solver"""
+    if len(sys.argv) != 2:
+        print_usage_and_exit()
 
-    size = int(argv[1])
-    chess_board = [["$" for y in range(size)] for x in range(size)]
-    print(chess_board)
-    print("{:d}".format(int(argv[1])))
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print_error_and_exit("N must be a number")
+
+    if N < 4:
+        print_error_and_exit("N must be at least 4")
+
+    solve_nqueens(N)
+
+
+if __name__ == "__main__":
+    main()
