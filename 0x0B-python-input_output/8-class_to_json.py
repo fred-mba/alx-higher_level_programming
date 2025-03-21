750f645e73357bbe25a7dd068305d91c4830abd7
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-03-21 22:04:46 +0300

    Class to JSON

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -6,4 +6,6 @@ def class_to_json(obj):
     """Returns the dictionary description with simple data structure
        for JSON serialization of an object
     """
-    return obj.__dict__
+    if hasattr(obj, "__dict__"):
+        return obj.__dict__
+    return {}
