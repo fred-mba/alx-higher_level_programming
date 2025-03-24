19ad7021c780de368cd3086de4df5a76861eb10c
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-03-24 16:48:21 +0300

    Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,16 @@ class Rectangle(Base):
         """Defines how the Rectangle object is represented in human-readable"""
         return f"[Rectangle] ({self.id}) \
 {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+
+    def update(self, *args):
+        """"Assigns an argument to each attribute"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
