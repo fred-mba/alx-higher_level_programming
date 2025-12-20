1cd4685b2e545ee38f19e494828b854ccbcaaac3
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-12-20 16:20:08 +0300

    User execution permission granted

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+/* Creates a new list with its element multiplied by the index in the list */
+
 const list = require('./100-data').list;
-const newList = list.map((x, y) => x * y);
+const newList = list.map((x, index) => x * index);
 
 console.log(list);
 console.log(newList);
