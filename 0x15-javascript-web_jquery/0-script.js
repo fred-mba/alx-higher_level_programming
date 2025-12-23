e6d0748d4e35a8901c9183c5eefb2c094e1dcd0a
Fredrick Mbanga <fredrickmbangah68@gmail.com>
2025-12-23 12:11:11 +0300

    Better version

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,8 +1,5 @@
 /**
- * JavaScript script that updates the text color of the <header> element to red (#FF0000)
+ * Updates the text color of the <header> element to red (#FF0000)
  * Must use document.querySelector to select the HTML tag
 */
-const headerElement = document.querySelector('header');
-if (document.querySelector('header')) {
-  headerElement.style.color = '#FF0000';
-}
+document.querySelector("header").style.color = "#FF0000"
